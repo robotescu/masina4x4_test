73a5e5710bf2110438eef24aa38c0fe5fcf15f68
robotescu <80849623+robotescu@users.noreply.github.com>
2025-05-20 20:37:16 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,30 @@
-input.onButtonPressed(Button.A, function () {
-    wuKong.mecanumRun(wuKong.RunList.Front, 100)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-    wuKong.mecanumRun(wuKong.RunList.rear, 100)
-    basic.pause(1000)
-    wuKong.mecanumStop()
-    wuKong.mecanumRun(wuKong.RunList.right, 100)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-    wuKong.mecanumRun(wuKong.RunList.left, 100)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-})
-input.onButtonPressed(Button.B, function () {
-    wuKong.mecanumSpin(wuKong.TurnList.Left, 100)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-    wuKong.mecanumSpin(wuKong.TurnList.Right, 100)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-    wuKong.mecanumDrift(wuKong.TurnList.Left)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-    wuKong.mecanumDrift(wuKong.TurnList.Right)
-    basic.pause(2000)
-    wuKong.mecanumStop()
-})
+PlanetX_AILens.initModule()
 wuKong.mecanumWheel(
 wuKong.ServoList.S1,
 wuKong.ServoList.S2,
 wuKong.ServoList.S3,
 wuKong.ServoList.S4
 )
-basic.showIcon(IconNames.Happy)
+PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Card)
+basic.showIcon(IconNames.Tortoise)
+basic.forever(function () {
+    PlanetX_AILens.cameraImage()
+    if (PlanetX_AILens.trafficCard(PlanetX_AILens.trafficCards.forward)) {
+        wuKong.mecanumRun(wuKong.RunList.Front, 100)
+        basic.pause(1000)
+    } else if (PlanetX_AILens.trafficCard(PlanetX_AILens.trafficCards.turnleft)) {
+        wuKong.mecanumRun(wuKong.RunList.left, 100)
+        basic.pause(500)
+    } else if (PlanetX_AILens.trafficCard(PlanetX_AILens.trafficCards.turnright)) {
+        wuKong.mecanumRun(wuKong.RunList.right, 100)
+        basic.pause(500)
+    } else if (PlanetX_AILens.trafficCard(PlanetX_AILens.trafficCards.back)) {
+        wuKong.mecanumSpin(wuKong.TurnList.Right, 100)
+        basic.pause(1000)
+    }
+    wuKong.mecanumStop()
+    for (let index = 0; index < 3; index++) {
+        PlanetX_AILens.cameraImage()
+    }
+    basic.pause(500)
+})
